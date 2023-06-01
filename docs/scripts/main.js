3cfd8d95af43a4f582f4d2cf94c1987bbc26923b
Vamdever <134436770+Vamdever@users.noreply.github.com>
2023-06-01 19:38:04 +0000

    vamos ver se isso da certo

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,5 +2,27 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Olá mundo!";
 
+function falaOi() {
+ titulo.style.backgroundColor='white;'
+}
 
 
+const myImage = document.querySelector("img");
+
+
+
+
+function mudaImagem(){
+
+    myImage.onclick = () => {
+      if (ImageBitmap.getAttribute("src") =="imagens/SalaoGrande.jpg") {
+        ImageBitmap.getAttribute("src",imagens/Castelo2.jpg);
+        ImageBitmap.style.backgroundColor=white
+      }
+    };
+    
+
+
+
+
+}
